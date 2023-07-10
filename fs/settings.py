740d8b71034d4f1cb30b9e75628d6a514c7b9f76
Ahmet Erturk <aesatiroglu@gmail.com>
2023-07-10 19:32:32 +0300

    deployment

--- fs/settings.py
+++ fs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1(d-%g&fydx#vu(t865^@@glgx1r@hsle%y4c^4-!dhfanm1dp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
